2de121608ada2ed3f686645bc90936324c000c01
AFSHAR <AFSHAR@DESKTOP-H088APG>
2024-11-02 23:34:02 +0330

    Add project files.

---
+++ Tamrin4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Yek adad vard konid: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            Console.WriteLine($"shoma adad ra vared kardid: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Voroudi na motabar ast lotfan ye adad motabar vared konid.");
+        }
+    }
+}
